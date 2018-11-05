e90322d86fffb273de839c0890abc795cbd82dfe
LeoLau <liuyuansix@163.com>
2018-11-05 18:32:45 +0800

    继续完成注册页面

---
+++ config/default.js
@@ -0,0 +1,16 @@
+const mac = {
+    //启动端口
+    port: 3000,
+
+    database:{
+        DATABASE:'nodesql',
+        USERNAME:'root',
+        PASSWORD:'wolaile',
+        PORT:'8889',
+        HOST:'localhost'
+    }
+}
+
+const config = mac
+
+module.exports = config
---
+++ index.js
@@ -0,0 +1,46 @@
+const Koa = require('koa')
+const path = require('path')
+const bodyParser = require('koa-bodyparser')
+const ejs = require('ejs')
+const session = require('koa-session-minimal')
+const MysqlStore = require('koa-mysql-session')
+const config = require('./config/default.js')
+const router = require('koa-router')
+const views = require('koa-views')
+const staticCache = require('koa-static-cache')
+const app = new Koa()
+
+
+//session存储配置
+const sessionMysqlConfig = {
+    user:config.database.USERNAME,
+    password:config.database.PASSWORD,
+    database:config.database.DATABASE,
+    host:config.database.HOST,
+    port:config.database.PORT
+}
+
+//配置session中间件
+app.use(session({
+    key:"USER_SID",
+    store: new MysqlStore(sessionMysqlConfig)
+}))
+
+//缓存
+app.use(staticCache(path.join(__dirname, './public'),{dynamic:true},{maxAge: 365 * 24 * 60 * 60}))
+
+app.use(staticCache(path.join(__dirname, './images'), {dynamic:true},{maxAge: 356 * 24 * 60 * 60}))
+
+//配置服务器端模板渲染引擎中间件
+app.use(views(path.join(__dirname, './views'), {
+    extension: 'ejs'
+}))
+app.use(bodyParser({
+    formLimit: '1mb'
+}))
+
+app.use(require('./routers/signup.js').routes())
+
+app.listen(config.port)
+
+console.log(`listening on port ${config.port}`)
